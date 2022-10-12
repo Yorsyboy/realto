9c715252c4b07d84892297b8d2dfcf4a1cc96a34
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-10-12 14:12:46 +0100

    Configure tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <header>
+            <div>
+                <img className="h-5" src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="" />
+            </div>
+        </header>
+    </div>
+  )
+}
