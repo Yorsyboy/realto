c571fec4646c73dbc19c9b52de8a26a5497da628
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-10-18 19:30:54 +0100

    Style inputs Form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -31,14 +33,33 @@ export default function SignIn() {
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
-            <input 
-            className="w-full" 
-            type="email" 
-            id="email" 
-            placeholder="Email Address" 
-            value={email}
-            onChange={onChange}
+            <input
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              type="email"
+              id="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={onChange}
             />
+            <div className="relative mb-6">
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={onChange}
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible className="absolute right-3 top-3 text-xl cursor-pointer" 
+                onClick={() => setShowPassword
+                  ((prevState) => !prevState)} />
+              ) : (
+                <AiFillEye className="absolute right-3 top-3 text-xl cursor-pointer" 
+                onClick={() => setShowPassword
+                  ((prevState) => !prevState)} />
+              )}
+            </div>
           </form>
         </div>
       </div>
