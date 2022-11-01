7c6a5a46d6863dc96db5045448db903f71824fd7
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-11-01 22:55:27 +0100

    Implement Edit function

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [ changeDetail, setChangeDetail ] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -12,10 +16,38 @@ export default function Profile() {
 
   const { name, email } = formData;
 
+  const handleChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        //update display name in firestore
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+      toast.success("Profile updated successfully");
+      
+    } catch (error) {
+      toast.error("Error updating profile");
+    }
+  }
+
   const Logout = () => {
     auth.signOut();
     navigate("/");
-  }
+  };
   return (
     <>
       <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
@@ -26,8 +58,10 @@ export default function Profile() {
               type="text"
               id="name"
               value={name}
-              disabled
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              disabled={!changeDetail}
+              onChange={handleChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out
+              ${changeDetail && "bg-red-200 focus:bg-red-200"}`} // if changeDetail is true, then the background color will be red
             />
 
             <input
@@ -39,13 +73,24 @@ export default function Profile() {
             />
 
             <div className="mb-6 flex justify-between whitespace-nowrap text-sm sm:text-lg">
-              <p className="flex items-center">Do you want to change your name?
-                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
+              <p className="flex items-center">
+                Do you want to change your name?
+                <span 
+                onClick={() => {
+                  changeDetail && handleSubmit(); // if changeDetail is true, then call onsubmit()
+                  setChangeDetail((prevState) => !prevState);
+                }}
+                className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  {changeDetail ? "Apply Change" : "Edit"}
+                </span>
               </p>
 
-              <p 
-              onClick={Logout}
-              className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer">Sign Out</p>
+              <p
+                onClick={Logout}
+                className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer"
+              >
+                Sign Out
+              </p>
             </div>
           </form>
         </div>
