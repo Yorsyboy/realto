ab971d217583e8fc46cb181da5e20da44843d771
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-11-01 22:13:45 +0100

    Check for authenticated user

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
+import {useAuthStatus} from '../hooks/useAuthStatus'
 
 export default function PrivateRoute() {
-    const loggedIn = false;
-  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />
+    const { LoggedIn, checkStatus } = useAuthStatus();
+    if (checkStatus) {
+        return <div>Loading...</div>
+    }
+  return LoggedIn ? <Outlet /> : <Navigate to="/sign-in" />
 }
