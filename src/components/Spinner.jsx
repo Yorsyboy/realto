c25044ad932ceda36f1a222f2d0a940e8c1d7432
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-11-02 08:37:46 +0100

    Implement spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import spinner from '../assets/svg/spinner.svg'
+
+export default function Spinner() {
+  return (
+    <div className="bg-black bg-opacity-50 flex justify-center items-center fixed left-0 right-0 bottom-0 top-0 z-50">
+        <div>
+            <img 
+            className="h-24"
+            src={spinner} alt="Loading...." />
+        </div>
+    </div>
+  )
+}
