36fa66bc01abedb8c84da33f072d987a2fe142db
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-11-24 00:00:45 +0100

    Fix responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
-            className="h-5 cursor-pointer"
+            className="h-3 cursor-pointer md:h-5 lg:h-6"
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
             alt="logo"
             onClick={() => navigate("/")}
@@ -55,14 +55,13 @@ export default function Header() {
               Offers
             </li>
             <li
-              className={`cursor-pointer py-2 text-sm font-semibold
+              className={`cursor-pointer py-3 text-sm font-semibold
             text-gray-400 border-b-[3px] border-b-transparent
             ${(path("/sign-in") || path("/profile")) && "text-black border-b-red-500"}`}
               onClick={() => navigate("/profile")}
             >
-              <button className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-slate-200 hover:text-red-500">
+             
                 {pageState}
-              </button>
             </li>
           </ul>
         </div>
