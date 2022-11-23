9f64415b23bf8b263ed407eff002fcc0d39ed32b
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-11-24 00:52:33 +0100

    Fix inters errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 import Profile from './pages/Profile';
@@ -7,7 +8,6 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Header from './components/Header';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from './pages/CreateListing';
 import EditListing from './pages/EditListing';
@@ -21,7 +21,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/profile' element={<PrivateRoute />}>
+          <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
@@ -29,10 +29,10 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="/create-listing" element={<PrivateRoute />} >
+          <Route path="/create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
-          <Route path="/edit-listing" element={<PrivateRoute />} >
+          <Route path="/edit-listing" element={<PrivateRoute />}>
             <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
           <Route path="/category/:categoryName/:listingId" element={<Listing />} />
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAucey18Ys6gJuuKfqGMjak6lfewtTzkO4",
-  authDomain: "realtor-clone-910a8.firebaseapp.com",
-  projectId: "realtor-clone-910a8",
-  storageBucket: "realtor-clone-910a8.appspot.com",
-  messagingSenderId: "289377162626",
-  appId: "1:289377162626:web:2edd0d16877e7a15686b9b"
+  apiKey: 'AIzaSyAucey18Ys6gJuuKfqGMjak6lfewtTzkO4',
+  authDomain: 'realtor-clone-910a8.firebaseapp.com',
+  projectId: 'realtor-clone-910a8',
+  storageBucket: 'realtor-clone-910a8.appspot.com',
+  messagingSenderId: '289377162626',
+  appId: '1:289377162626:web:2edd0d16877e7a15686b9b',
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
+/* eslint-disable import/prefer-default-export */
 export const db = getFirestore();
